1e15ffbc89b696a17da249395d0855d363b641c4
Andry Saprigin <coffeei.2002@gmail.com>
2023-12-03 14:13:14 +0200

    injection from xml

--- src/main/java/com/example/springcourse/App.java
+++ src/main/java/com/example/springcourse/App.java
@@ -3,10 +3,15 @@ package com.example.springcourse;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+    /**
+     * There is 3 way to set bean
+     * 1. From XML file
+     */
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");) {
-            TestBean bean = context.getBean("testBean", TestBean.class);
-            System.out.println(bean.getName());
+            Music music = context.getBean("musicBean", Music.class);
+            MusicPlayer musicPlayer = new MusicPlayer(music);
+            musicPlayer.play();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
---
+++ src/main/java/com/example/springcourse/MusicPlayer.java
@@ -0,0 +1,13 @@
+package com.example.springcourse;
+
+public class MusicPlayer {
+    private Music music;
+
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void play() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
