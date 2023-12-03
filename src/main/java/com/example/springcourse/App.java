e286ed0f35def720306d8ce6ca1c2114db4ac379
Andry Saprigin <coffeei.2002@gmail.com>
2023-12-03 18:15:04 +0200

    make HomeTask

--- src/main/java/com/example/springcourse/App.java
+++ src/main/java/com/example/springcourse/App.java
@@ -1,5 +1,7 @@
 package com.example.springcourse;
 
+import javax.sound.midi.SysexMessage;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
@@ -9,11 +11,36 @@ public class App {
      */
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");) {
-            Music music = context.getBean("musicBean", Music.class);
-            MusicPlayer musicPlayer = new MusicPlayer(music);
-            musicPlayer.play();
+            // #region Add first level bean from constructor
+            firstLevelBean(context);
+            // #endregion
+
+            // #region Add second level bean from
+            secondLevelBean(context);
+            // #endregion
+
+            // #region Add second level bean from props using props file
+            injectionFromPropUsingProp(context);
+            // #endregion
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void firstLevelBean(ClassPathXmlApplicationContext context) {
+        Music music = context.getBean("musicBean", Music.class);
+        new MusicPlayer(music).play();
+    }
+
+    private static void secondLevelBean(ClassPathXmlApplicationContext context) {
+        MusicPlayer player = context.getBean("musicPlayerBean", MusicPlayer.class);
+        player.play();
+    }
+
+    private static void injectionFromPropUsingProp(ClassPathXmlApplicationContext context) {
+        MusicPlayer player = context.getBean("musicPlayerBean", MusicPlayer.class);
+        player.play();
+        System.out.println(player.getName());
+        System.out.println(player.getVolume());
     }
 }
--- src/main/java/com/example/springcourse/MusicPlayer.java
+++ src/main/java/com/example/springcourse/MusicPlayer.java
@@ -3,8 +3,42 @@ package com.example.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    // #region getters
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+    // #endregion
+
+    // #region setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+    // #endregion
+
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+
+    public MusicPlayer() {
     }
 
     public void play() {
